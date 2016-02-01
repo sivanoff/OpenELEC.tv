7ac7a50e48c30c920f144325d89fc3c19ac3b040
kszaq <kszaquitto@gmail.com>
2016-02-01 03:12:30 +0100

    packages/linux: Use my repo for Amlogic kernel
    And update kernel to 3.10-9961e21

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -31,8 +31,9 @@ PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and
 PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   amlogic)
-    PKG_VERSION="amlogic-3.10-716f179"
-    PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+    LINUX_VERSION="3.10-9961e21"
+    PKG_VERSION="amlogic-$LINUX_VERSION"
+    PKG_URL="https://github.com/kszaq/linux/releases/download/$LINUX_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   imx6)
     PKG_VERSION="cuboxi-3.14-ea83bda"
