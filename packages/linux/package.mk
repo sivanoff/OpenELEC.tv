dd30a724c8010630ff9d008b2d3a5542790405fa
kszaq <kszaquitto@gmail.com>
2016-03-16 23:43:00 +0100

    packages/linux: update Amlogic kernel to 3.10-cf51189

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -31,9 +31,9 @@ PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and
 PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   amlogic)
-    LINUX_VERSION="3.10-1524879"
+    LINUX_VERSION="3.10-cf51189"
     PKG_VERSION="amlogic-$LINUX_VERSION"
-    PKG_URL="https://github.com/kszaq/linux/releases/download/$LINUX_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_URL="http://s805.eu/sources/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   imx6)
     PKG_VERSION="cuboxi-3.14-ea83bda"
