dc70dd1002464da4b8f71d0576a88da66c2a04cb
kszaq <kszaquitto@gmail.com>
2016-04-03 00:03:24 +0200

    packages/linux: update Amlogic kernel to 3.10-61a38cb

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -31,7 +31,7 @@ PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and
 PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   amlogic)
-    LINUX_VERSION="3.10-cf51189"
+    LINUX_VERSION="3.10-61a38cb"
     PKG_VERSION="amlogic-$LINUX_VERSION"
     PKG_URL="http://s805.eu/sources/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
